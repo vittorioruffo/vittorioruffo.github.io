d1772c3ee9e4f4998aff612c4ded6a7b0a03078c
vittorioruffo <68504626+vittorioruffo@users.noreply.github.com>
2025-10-08 18:25:49 +0000

    Deploying to gh-pages from @ vittorioruffo/vittorioruffo.github.io@fc3603aa72b9ef06f32b365238f997c6078510d5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Teaching and academic experience.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
