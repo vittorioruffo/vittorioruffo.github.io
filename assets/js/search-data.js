0ad0736beea674152959bfcf9a1ad853b6be86a5
vittorioruffo <68504626+vittorioruffo@users.noreply.github.com>
2025-10-08 19:09:40 +0000

    Deploying to gh-pages from @ vittorioruffo/vittorioruffo.github.io@9e5e40c78db2e518bcf005e18750eece33f204c7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,8 +23,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
+        },{id: "nav-",
+          title: "",
           description: "",
           section: "Navigation",
           handler: () => {
